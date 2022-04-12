eb1f1f3480adb3085a3e872c63e62c2fa39b01c1
TDiaz319 <acupuntista@gmail.com>
2022-04-12 13:10:05 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "menu", pathMatch:"full"},
+  { path: "menu", component: MainMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/entidades/producto.ts
@@ -0,0 +1,6 @@
+
+export interface Producto {
+    id: number;
+    nombreProducto: string;
+    cantidad: number;
+}
