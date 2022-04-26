e2e720bbd8a5da8b6088b32260720a7a122c088c
TDiaz319 <acupuntista@gmail.com>
2022-04-26 21:24:16 +0200

    botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ReservasComponent } from './reservas/reservas.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "menu", pathMatch: "full" },
-  { path: "menu", component: MainMenuComponent }
+  { path: "menu", component: MainMenuComponent },
+  { path: "productos", component:ProductosComponent},
+  //{ path: "pedidos", component:ProductosComponent}
+  { path: "reservas", component:ReservasComponent}
 ];
 
 @NgModule({
