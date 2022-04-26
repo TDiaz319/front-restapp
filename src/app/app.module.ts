ec95059a710107730f36f83dcd5241459c695a0f
TDiaz319 <acupuntista@gmail.com>
2022-04-26 16:59:30 +0200

    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,25 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductosComponent } from './productos/productos.component';
 import { ProductosService } from './services/productos.service';
+import { InicioComponent } from './inicio/inicio.component';
+import { ReservasComponent } from './reservas/reservas.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "menu", pathMatch: "full" },
-  { path: "menu", component: MainMenuComponent }
+  { path: "menu", component: MainMenuComponent },
+  { path: "", redirectTo: "inicio", pathMatch: "full" },
+  { path: "inicio", component: InicioComponent },
+  { path: "", redirectTo: "reservas", pathMatch: "full" },
+  { path: "reservas", component: ReservasComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    ProductosComponent
+    ProductosComponent,
+    InicioComponent,
+    ReservasComponent
   ],
   imports: [
     BrowserModule,
