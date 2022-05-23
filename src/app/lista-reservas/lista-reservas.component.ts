839f073dfc5acde20957937ff08785c6b0578177
TDiaz319 <acupuntista@gmail.com>
2022-05-23 17:49:53 +0200

    lista reserva

--- src/app/lista-reservas/lista-reservas.component.ts
+++ src/app/lista-reservas/lista-reservas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Reserva } from '../entidades/reserva';
+import { ReservaService } from '../services/reserva.service';
 
 @Component({
   selector: 'app-lista-reservas',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaReservasComponent implements OnInit {
 
-  constructor() { }
+  reserva: Reserva[] = [];
+  checkoutForm: any;  
+
+  constructor(private formBuilder: FormBuilder,
+    private reservaService : ReservaService) { 
+
+   }
+  
 
   ngOnInit(): void {
   }
 
+  onSubmit(reservaData: Reserva) {
+    console.log(reservaData);
+ 
+  }
+  /*
+  buscarReservas (id: string){
+    this.reservaService.buscarReserva(id).subscribe(
+      response =>{
+        this.reserva= response as Reserva[];
+        console.log(response);
+      }
+    );
+  }*/
+
+  recargaTabla(event:any) {
+    console.log(event.target.value);
+    //this.buscarReservas(event.target.value);
+  }
+
 }
+
+
